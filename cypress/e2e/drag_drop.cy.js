0cde61578bcecd86cde38ffeb83b3f8bc3cd67bc
Alexandre DERET <[allex.deret@gmail.com](mailto:allex.deret@gmail.com)>
2023-05-11 17:18:28 +0200

    fichier telechargé

--- cypress/e2e/drag_drop.cy.js
+++ cypress/e2e/drag_drop.cy.js
@@ -18,35 +18,43 @@ describe('template spec', () => {
                 // Soucis click simple / double ?
                 cy.get('.pp__screens-timeline__add-screen-cta > .ds__button-wrapper').click();
                 //cy.get('.pp__screens-timeline__add-screen-cta > .ds__button-wrapper').click();
-                cy.get(':nth-child(1) > .pp__add-a-new-screen-screens-list__category-screens > :nth-child(8) > .pp__add-a-new-screen-catalog-item > .pp__add-a-new-screen-catalog-item__cover').click();
+                cy.get(':nth-child(1) > .pp__add-a-new-screen-screens-list__category-screens > :nth-child(9) > .pp__add-a-new-screen-catalog-item > .pp__add-a-new-screen-catalog-item__cover').click();
                 cy.get('[data-cy="projectNavBar_title_textInput"]').type("Groupe 1 - Cypress");
                 cy.get('[data-cy="settingsPanel_library_button"]').click();
                 cy.get('[data-cy="library_enterpriseStock_tab"]').click();
                 cy.get('[data-heap-label="pp__stock-filters__gif"]').click();
-                cy.get('[data-placeholder="Running with podcasts"]').type("npx percy exec --cypress run");
-                cy.get('.search-input__input').type("panda");
+                cy.get('[data-placeholder="New store opening"]').type("Github (encore) en panne");
+                cy.get('.search-input__input').type("cartman rage");
                 cy.get(".search-input__icon").click();
-                cy.get(':nth-child(3) > .media-thumbnail > .media-wrapper > .media-title-tooltip > .media-image-item').trigger('dragstart');
+                cy.get(':nth-child(1) > .media-thumbnail > .media-wrapper > .media-title-tooltip > .media-image-item').trigger('dragstart');
                 cy.get('.upload-container')
                   .trigger('dragenter', { force: true })
                   .trigger('dragover', { force: true })
                   .trigger('drop', { force: true })
                 cy.get('[data-cy="mediaEdition_keepSize_toggle"] > .ds__switcher__bar').click();
                 cy.get('[data-cy="mediaEditionModal_save_button"]').click();
-                cy.get(".add-a-media__button-container > .has-tooltip").click();
-                cy.get('.search-input__input').clear();
-                cy.get('.search-input__input').type("issou");
-                cy.get(".search-input__icon").click();
-                cy.get(':nth-child(4) > .media-thumbnail > .media-wrapper > .media-title-tooltip > .media-image-item').trigger('dragstart');
-                cy.get('.upload-container')
-                  .trigger('dragenter', { force: true })
-                  .trigger('dragover', { force: true })
-                  .trigger('drop', { force: true })
-                cy.get('[data-cy="mediaEdition_keepSize_toggle"] > .ds__switcher__bar').click();
-                cy.get('[data-cy="mediaEditionModal_save_button"]').click();
+                // SI on veut une deuxième vidéo (plus tard)
+                // cy.get(".add-a-media__button-container > .has-tooltip").click();
+                // cy.get('.search-input__input').clear();
+                // cy.get('.search-input__input').type("issou");
+                // cy.get(".search-input__icon").click();
+                // cy.get(':nth-child(4) > .media-thumbnail > .media-wrapper > .media-title-tooltip > .media-image-item').trigger('dragstart');
+                // cy.get('.upload-container')
+                //   .trigger('dragenter', { force: true })
+                //   .trigger('dragover', { force: true })
+                //   .trigger('drop', { force: true })
+                // cy.get('[data-cy="mediaEdition_keepSize_toggle"] > .ds__switcher__bar').click();
+                // cy.get('[data-cy="mediaEditionModal_save_button"]').click();
                 cy.get('[data-heap-label="pp__generate-button__button-action-medium"]').click();
                 cy.get('[data-cy="globalPreview_generate_button"]').click();
-                cy.get('[data-cy="projectView_approvalProcessGetApproval_button"]', { timeout: 15000}).should("be.visible").click();
+                cy.get('[data-cy="projectView_approvalProcessGetApproval_button"]', { timeout: 20000}).should("be.visible").click();
                 cy.get('[data-cy="modal_confirm_button"]', { timeout: 15000}).should("be.visible").click();
+                cy.get('.pp__projects-list > :nth-child(1)').should("contain", "Groupe 1 - Cypress");
+                cy.get(':nth-child(1) > .pp__media-card__content-wrapper > .pp__media-card__content-container > .pp__project-card__draft-thumbnail-container > .pp__project-card__thumbnail').click();
+                cy.get('[data-cy="shareLink_approvalProcessApprove_button"]').should("be.visible").click();
+                cy.get('[data-cy="modal_confirm_button"]').should("be.visible").click();
+                cy.get('.pp__projects-list > :nth-child(1)').should("contain", "Groupe 1 - Cypress");
+                cy.get('.pp__project-card__video').click();
+                cy.get('[data-cy="projectView_download_button"]').click();
         })
 })
